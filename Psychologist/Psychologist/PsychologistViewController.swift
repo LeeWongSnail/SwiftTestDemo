f64c91be3268c61729b55c131590b7ec1b7c2deb
LeeWongSnail <@wangli_0632@163.com>
2016-02-17 15:13:52 +0800

    SplitViewController or NavigationController

---
+++ Psychologist/Psychologist/PsychologistViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PsychologistViewController.swift
+//  Psychologist
+//
+//  Created by LeeWong on 16/2/17.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+    
+    
+    @IBAction func nothing(sender: UIButton) {
+        performSegueWithIdentifier("nothing", sender: nil)
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        var destination = segue.destinationViewController as? UIViewController
+        if let navCon = destination as? UINavigationController {
+            destination = navCon.visibleViewController
+        }
+        
+        if let hvc = destination as? HappinessViewController {
+            if let identifier = segue.identifier {
+                switch identifier {
+                    case "sad": hvc.happiness = 0
+                    case "happy": hvc.happiness = 100
+                    case "nothing":hvc.happiness = 25
+                    default : hvc.happiness = 50
+                }
+            }
+        }
+    }
+}
